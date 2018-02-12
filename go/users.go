90b5c38691a450778bf37c8829a55fbe6325133f
Роман Киселёв <imail.for.iwork@gmail.com>
2018-02-12 19:53:35 +0700

    big int works prefect

--- go/users.go
+++ go/users.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"openprovider-back/go/models"
 	"math/big"
+	"errors"
 )
 
 type Users struct {
@@ -22,13 +23,14 @@ func GetTribonacсiValue(w http.ResponseWriter, r *http.Request) {
 		argument, err := obtainArgument(path)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-			e := models.ErrorMessage{ Code: "400", Message: "Invalid argument"}
+			e := models.ErrorMessage{ Code: "400", Message: "Invalid argument: " + err.Error()}
 			errorMessage, _ := json.Marshal(e)
 			http.Error(w, string(errorMessage) , http.StatusBadRequest)
 			return
 		}
 
-		result := tribonacciThroughCache(argument)
+		result := TribonacciThroughCache(argument)
+
 
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, result)
@@ -37,22 +39,33 @@ func GetTribonacсiValue(w http.ResponseWriter, r *http.Request) {
 func obtainArgument(path string) (int, error) {
 	fragments := strings.Split(path,"/")
 	lastFragment := fragments[len(fragments)-1]
-	return strconv.Atoi(lastFragment)
+	argument, err := strconv.Atoi(lastFragment)
+
+	if argument < 0 {
+		err = errors.New("negative arguments not supported")
+	}
+
+	//if argument > 100000 {
+	//	err = errors.New("аргументы больше 100000 запрещены в демонстрационных целях")
+	//}
+
+	return argument, err
 }
 
-func tribonacciThroughCache(argument int ) string {
-	result := tribonacсiIteroBig(argument)
+func TribonacciThroughCache(argument int ) string {
+	result := TribonacсiIteroBig(argument)
 	return  result.String()
 }
 
 
-func tribonacсiIteroBig(argument int) *big.Int {
+func TribonacсiIteroBig(argument int) *big.Int {
+	var zero = new(big.Int).SetUint64(0)
 	var first = new(big.Int).SetUint64(0)
 	var second = new(big.Int).SetUint64(1)
 	var third = new(big.Int).SetUint64(1)
 
 	if argument == 0 {
-		return new(big.Int).SetUint64(0)
+		return zero
 	}else if argument == 1 {
 		return first
 	}else if argument == 2 {
@@ -60,15 +73,23 @@ func tribonacсiIteroBig(argument int) *big.Int {
 	}else if argument == 3 {
 		return third
 	}else {
-		next := first.Add(first, second.Add(second,third))
-
+		next := new(big.Int).SetUint64(2)
 		stepsToDone := argument-3 // опускаем уже просуммированные члены
+
 		for i := 1; i < stepsToDone; i++ {
+
+			// смещаемся по последовательности на один шаг вперёд
 			first = second
 			second = third
 			third = next
-			next = first.Add(first, second.Add(second,third)) // вычисляем следующий член последовательности
+
+			// суммируем все три члена
+			sf := new(big.Int).Add(first,second)
+			tsf := new(big.Int).Add(sf,third)
+
+			next = tsf // запоминаем
 		}
+
 		return next
 	}
 }
