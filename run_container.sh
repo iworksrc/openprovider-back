c5fd32d799c5d5aad775200e3424f8e06bab8032
Роман Киселёв <imail.for.iwork@gmail.com>
2018-02-13 02:12:01 +0700

    pre release 1.0

--- run_container.sh
+++ run_container.sh
@@ -9,16 +9,4 @@ if [ -z $IMAGE ];then
     exit 1
 fi
 
-#CONTAINER_ID=$(sudo docker ps -a --filter="name=$NAME" -q)
-#
-#echo "CONTAINER_ID="$CONTAINER_ID
-#
-#if [[ $CONTAINER_ID > "" ]];then
-#    echo "container $NAME is present, try run..."
-#    sudo docker run $NAME
-#else
-#    echo "conteiner with name $NAME not found, create&run..."
-#    sudo docker run -h localhost -p 8080:8080 --name $NAME $NAME:latest
-#fi
-
 sudo docker run --rm -h localhost -p 8080:8080 --name $NAME $NAME:latest
